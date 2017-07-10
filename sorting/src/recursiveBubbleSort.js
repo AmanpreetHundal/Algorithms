a82d4982d5e8f0c9e6511f28b279f53f52d9c65b
Amanpreet Kaur Hundal (Infosys Ltd) <v-amhund@microsoft.com>
2017-07-10 14:34:48 -0700

    working on recursive algo

--- sorting/src/recursiveBubbleSort.js
+++ sorting/src/recursiveBubbleSort.js
@@ -1,18 +1,14 @@
-function recursiveBubbleSort(array,index) {
-    if((array[index] > array[index+1])) {
-        [array[index],array[index+1]] = [array[index+1],array[index]];
-    }
-    if (index == array.length-1) {
+function recursiveBubbleSort(array,index = 0) {
+    if (index == array.length) {
         return array;
     }
-    else {
-        recursiveBubbleSort(array,index+1)
+    if(array[index] > array[index+1]) {
+        [array[index],array[index+1]] = [array[index+1],array[index]];
+        return array;
     }
-    for (var i=0; i< array.length; i++){        
-        for (var j=0; j<array.length-1; j++) {            
-            if(array[j] > array[j+1]){                
-              [array[j],array[j+1]] = [array[j+1],array[j]];
-            }
+    for (var i=0; i<array.length; i++) {
+        while (index < array.length) {
+            array = bubbleSort1(array,index+1)
         }
     }
     return array;
